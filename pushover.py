37b7d6b8f8cae3d67deca9683854e11f0a14c8aa
Cornoualis <Cornoualis@users.noreply.github.com>
2022-01-04 10:42:50 +0100

    Little updates to make it work with python 3.9
    - I replaced an old print statement with a print function
    - I replaced an "iteritems" call with "item"
    
    I tried it and it works with python 3.9!
    
    Bonne journée! ;-)

--- pushover.py
+++ pushover.py
@@ -1,29 +1,60 @@
-# pushover 1.0
-#
-# Copyright (C) 2013-2018  Thibaut Horel <thibaut.horel@gmail.com>
+"""After being imported, the module must be initialized by calling the :func:`init`
+function with a valid application token before sending messages.
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+A typical use of the module looks like this::
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+    import pushover
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import time
+    pushover.init("token")
+    client = Client("client-id")
+    client.send_message("Hello!", title="Hello", priority=1)
+"""
+
 import requests
+import time
+
+__all__ = ["init", "get_sounds", "Client", "MessageRequest",
+           "InitError", "RequestError"]
 
 BASE_URL = "https://api.pushover.net/1/"
 MESSAGE_URL = BASE_URL + "messages.json"
 USER_URL = BASE_URL + "users/validate.json"
 SOUND_URL = BASE_URL + "sounds.json"
 RECEIPT_URL = BASE_URL + "receipts/"
-GLANCE_URL = BASE_URL + "glances.json"
 
+SOUNDS = None
+TOKEN = None
+
+def get_sounds():
+    """Fetch and return a list of sounds (as a list of strings) recognized by
+    Pushover and that can be used in a notification message.
+
+    The result is cached: a request is made to the Pushover server only
+    the first time this function is called.
+    """
+    global SOUNDS
+    if not SOUNDS:
+        request = Request("get", SOUND_URL, {})
+        SOUNDS = request.answer["sounds"]
+    return SOUNDS
+
+def init(token, sound=False):
+    """Initialize the module by setting the application token which will be
+    used to send messages. If ``sound`` is ``True`` also returns the list of
+    valid sounds by calling the :func:`get_sounds` function.
+    """
+    global TOKEN
+    TOKEN = token
+    if sound:
+        return get_sounds()
+
+class InitError(Exception):
+    """Exception which is raised when trying to send a message before
+    initializing the module.
+    """
+
+    def __str__(self):
+        return "Init the pushover module by calling the init function"
 
 class RequestError(Exception):
     """Exception which is raised when Pushover's API returns an error code.
@@ -38,197 +69,159 @@ class RequestError(Exception):
     def __str__(self):
         return "\n==> " + "\n==> ".join(self.errors)
 
-
-class Request(object):
+class Request:
     """Base class to send a request to the Pushover server and check the return
-    status code. The request is sent on instantiation and raises
+    status code. The request is sent on the instance initialization and raises
     a :class:`RequestError` exception when the request is rejected.
     """
 
-    def __init__(self, method, url, payload):
-        files = {}
-        if "attachment" in payload:
-            files["attachment"] = payload["attachment"]
-            del payload["attachment"]
-        self.payload = payload
-        self.files = files
-        request = getattr(requests, method)(url, params=payload, files=files)
+    def __init__(self, request_type, url, payload):
+        if not TOKEN:
+            raise InitError
+
+        payload["token"] = TOKEN
+        request = getattr(requests, request_type)(url, params=payload)
         self.answer = request.json()
         if 400 <= request.status_code < 500:
             raise RequestError(self.answer["errors"])
 
     def __str__(self):
-        return str(self.answer)
-
+        print(self.answer)
 
 class MessageRequest(Request):
-    """This class represents a message request to the Pushover API. You do not
-    need to create it yourself, but the :func:`Pushover.message` function
-    returns :class:`MessageRequest` objects.
+    """Class representing a message request to the Pushover API. You do not
+    need to create them yourself, but the :func:`Client.send_message` function
+    returns :class:`MessageRequest` objects if you need to inspect the requests
+    after they have been answered by the Pushover server.
 
     The :attr:`answer` attribute contains a JSON representation of the answer
-    made by the Pushover API. When sending a message with a priority of 2, you
-    can poll the status of the notification with the :func:`poll` function.
+    made by the Pushover API. In the case where you have sent a message with
+    a priority of 2, you can poll the status of the notification with the
+    :func:`poll` function.
     """
-
-    params = {
-        "expired": "expires_at",
-        "called_back": "called_back_at",
-        "acknowledged": "acknowledged_at",
-    }
 
     def __init__(self, payload):
         Request.__init__(self, "post", MESSAGE_URL, payload)
-        self.status = {"done": True}
+        self.receipt = None
         if payload.get("priority", 0) == 2:
-            self.url = RECEIPT_URL + self.answer["receipt"]
-            self.status["done"] = False
-            for param, when in MessageRequest.params.iteritems():
-                self.status[param] = False
-                self.status[when] = 0
+            self.receipt = self.answer["receipt"]
+        self.parameters = ["expired", "called_back", "acknowledged"]
+        for parameter in self.parameters:
+            setattr(self, parameter, False)
+            setattr(self, parameter + "_at", 0)
 
     def poll(self):
-        """If the message request has a priority of 2, Pushover keeps sending
-        the same notification until the client acknowledges it. Calling the
-        :func:`poll` function fetches the status of the :class:`MessageRequest`
-        object until the notifications either expires, is acknowledged by the
-        client, or the callback url is reached. The status is available in the
-        ``status`` dictionary.
+        """If the message request has a priority of 2, Pushover will keep
+        sending the same notification until the client acknowledges it. Calling
+        the :func:`poll` function will update the status of the
+        :class:`MessageRequest` object until the notifications either expires,
+        is acknowledged by the client, or the callback url is reached. The
+        attributes of interest are: ``expired, called_back, acknowledged`` and
+        their *_at* variants as explained in the API documentation.
 
-        Returns ``True`` when the request has expired or been acknowledged and
-        ``False`` otherwise so that a typical handling of a priority-2
-        notification can look like this::
+        This function returns ``None`` when the request has expired or been
+        acknowledged, so that a typical handling of a priority-2 notification
+        can look like this::
 
-            request = p.message("Urgent!", priority=2, expire=120, retry=60)
+            request = client.send_message("Urgent notification", priority=2)
             while not request.poll():
                 # do something
                 time.sleep(5)
-
-            print request.status
         """
-        if not self.status["done"]:
-            r = Request("get", self.url + ".json", {"token": self.payload["token"]})
-            for param, when in MessageRequest.params.iteritems():
-                self.status[param] = bool(r.answer[param])
-                self.status[when] = int(r.answer[when])
-            for param in ["acknowledged_by", "acknowledged_by_device"]:
-                self.status[param] = r.answer[param]
-            self.status["last_delivered_at"] = int(r.answer["last_delivered_at"])
-            if any(self.status[param] for param in MessageRequest.params):
-                self.status["done"] = True
-        return self.status["done"]
+        if (self.receipt and not any(getattr(self, parameter)
+                                     for parameter in self.parameters)):
+            request = Request("get", RECEIPT_URL + self.receipt + ".json", {})
+            for parameter in self.parameters:
+                setattr(self, parameter, request.answer[parameter])
+                setattr(self, parameter + "_at",
+                        request.answer[parameter + "_at"])
+            return request
 
-    def cancel(self):
-        """If the message request has a priority of 2, Pushover keeps sending
-        the same notification until it either reaches its ``expire`` value or
-        is aknowledged by the client. Calling the :func:`cancel` function
-        cancels the notification early.
-        """
-        if not self.status["done"]:
-            return Request(
-                "post", self.url + "/cancel.json", {"token": self.payload["token"]}
-            )
-        else:
-            return None
+class Client:
+    """This is the main class of the module. It represents a specific Pushover
+    user to whom messages will be sent when calling the :func:`send_message`
+    method.
 
-
-class Pushover(object):
-    """This is the main class of the module. It represents a Pushover app and
-    is tied to a unique API token.
-
-    * ``token``: Pushover API token
+    * ``user``: the Pushover's ID of the user.
+    * ``device``: if not ``None`` further ties the Client object to the
+      specified device.
     """
 
-    _SOUNDS = None
-    message_keywords = [
-        "title",
-        "priority",
-        "sound",
-        "callback",
-        "timestamp",
-        "url",
-        "url_title",
-        "device",
-        "retry",
-        "expire",
-        "html",
-        "attachment",
-    ]
-    glance_keywords = ["title", "text", "subtext", "count", "percent", "device"]
+    def __init__(self, user, device=None):
+        self.user = user
+        self.device = device
+        self.devices = []
 
-    def __init__(self, token):
-        self.token = token
-
-    @property
-    def sounds(self):
-        """Return a dictionary of sounds recognized by Pushover and that can be
-        used in a notification message.
+    def verify(self, device=None):
+        """Verify that the Client object is tied to an existing Pushover user
+        and fetches a list of this user active devices accessible in the
+        :attr:`devices` attribute. Returns a boolean depending of the validity
+        of the user.
         """
-        if not Pushover._SOUNDS:
-            request = Request("get", SOUND_URL, {"token": self.token})
-            Pushover._SOUNDS = request.answer["sounds"]
-        return Pushover._SOUNDS
-
-    def verify(self, user, device=None):
-        """Verify that the `user` and optional `device` exist. Returns
-        `None` when the user/device does not exist or a list of the user's
-        devices otherwise.
-        """
-        payload = {"user": user, "token": self.token}
+        payload = {"user": self.user}
+        device = device or self.device
         if device:
             payload["device"] = device
         try:
             request = Request("post", USER_URL, payload)
         except RequestError:
-            return None
-        else:
-            return request.answer["devices"]
+            return False
 
-    def message(self, user, message, **kwargs):
-        """Send `message` to the user specified by `user`. It is possible
-        to specify additional properties of the message by passing keyword
-        arguments. The list of valid keywords is ``title, priority, sound,
-        callback, timestamp, url, url_title, device, retry, expire and html``
-        which are described in the Pushover API documentation.
+        self.devices = request.answer["devices"]
+        return True
 
-        For convenience, you can simply set ``timestamp=True`` to set the
-        timestamp to the current timestamp.
-
-        An image can be attached to a message by passing a file-like object
-        to the `attachment` keyword argument.
+    def send_message(self, message, **kwords):
+        """Send a message to the user. It is possible to specify additional
+        properties of the message by passing keyword arguments. The list of
+        valid keywords is ``title, priority, sound, callback, timestamp, url,
+        url_title, device, retry and expire`` which are described in the
+        Pushover API documentation. For convenience, you can simply set
+        ``timestamp=True`` to set the timestamp to the current timestamp.
 
         This method returns a :class:`MessageRequest` object.
         """
+        valid_keywords = ["title", "priority", "sound", "callback",
+                          "timestamp", "url", "url_title", "device",
+                          "retry", "expire"]
 
-        payload = {"message": message, "user": user, "token": self.token}
-        for key, value in kwargs.iteritems():
-            if key not in Pushover.message_keywords:
+        payload = {"message": message, "user": self.user}
+        if self.device:
+            payload["device"] = self.device
+
+        for key, value in kwords.items():
+            if key not in valid_keywords:
                 raise ValueError("{0}: invalid message parameter".format(key))
-            elif key == "timestamp" and value is True:
-                payload[key] = int(time.time())
-            elif key == "sound" and value not in self.sounds:
-                raise ValueError("{0}: invalid sound".format(value))
-            else:
+
+            if key == "timestamp" and value:
+                payload[key] = time.time()
+            elif key == "sound":
+                if not SOUNDS:
+                    get_sounds()
+                if value not in SOUNDS:
+                    raise ValueError("{0}: invalid sound".format(value))
+                else:
+                    payload[key] = value
+            elif value:
                 payload[key] = value
 
         return MessageRequest(payload)
 
-    def glance(self, user, **kwargs):
-        """Send a glance to the user. The default property is ``text``, as this
-        is used on most glances, however a valid glance does not need to
-        require text and can be constructed using any combination of valid
-        keyword properties. The list of valid keywords is ``title, text,
-        subtext, count, percent and device`` which are  described in the
-        Pushover Glance API documentation.
 
-        This method returns a :class:`GlanceRequest` object.
-        """
-        payload = {"user": user, "token": self.token}
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Send a message to pushover.")
+    parser.add_argument("--token", help="Pushover application token",
+                        required=True)
+    parser.add_argument("--client", "-c", help="Pushover client ID",
+                        required=True)
+    parser.add_argument("message", help="message to send")
+    parser.add_argument("--title", "-t", help="message title")
+    parser.add_argument("--priority", "-p", help="message priority")
+    parser.add_argument("--url", help="additional url")
+    parser.add_argument("--url-title", help="additional url title")
 
-        for key, value in kwargs.iteritems():
-            if key not in Pushover.glance_keywords:
-                raise ValueError("{0}: invalid glance parameter".format(key))
-            else:
-                payload[key] = value
-
-        return Request("post", GLANCE_URL, payload)
+    args = parser.parse_args()
+    init(args.token)
+    Client(args.client).send_message(args.message, title=args.title,
+                                     priority=args.priority, url=args.url,
+                                     url_title=args.url_title, timestamp=True)
